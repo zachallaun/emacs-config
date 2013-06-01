660585d6e043f983453a59b2e58f5c3e1ac6c3fd
Zachary Allaun <zach.allaun@gmail.com>
2013-06-01 15:12:10 -0400

    Add marmalade as a package repository

--- user-init.el
+++ user-init.el
@@ -9,10 +9,12 @@
 ;;; Packages ;;;
 ;;;;;;;;;;;;;;;;
 
-;; MEPLA
+;; MEPLA and Marmalade
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
