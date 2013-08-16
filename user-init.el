2fb7252c6c164142bfbf5e9c6c7831f315aaea60
Zachary Allaun <zach.allaun@gmail.com>
2013-08-16 11:05:23 -0400

    C-x C-m for M-x

--- user-init.el
+++ user-init.el
@@ -198,6 +198,7 @@
 ;; smex - M-x Ido-like enhancement
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "C-x C-m") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command) ;; old M-x
 
