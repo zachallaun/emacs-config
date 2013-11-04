7bbfbcddedd6e79e29510b8cf8c3c1c629ae6ab3
Zachary Allaun <zach.allaun@gmail.com>
2013-11-04 15:04:07 -0500

    Smaller font because I can

--- init.el
+++ init.el
@@ -199,7 +199,7 @@
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; font family
-(set-default-font "Source Code Pro-11")
+(set-default-font "Source Code Pro-10")
 
 ;; the cursor should be a bar instead of a block
 (setq-default cursor-type 'bar)
