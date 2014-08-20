10d3abb32d2d4e6868f267968852e5b729a53fd7
Zachary Allaun <zach.allaun@gmail.com>
2014-08-20 13:39:49 -0400

    2 space indent in jsx mode

--- init.el
+++ init.el
@@ -664,7 +664,15 @@ prefix, send the form '(do (in-ns 'user) (refresh))."
 ;;-- init.jsx
 ;;----------------------------------------------------------------------------
 
-(custom-set-variables '(jsx-indent-level 2))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(jsx-indent-level 2))
 
 ;;----------------------------------------------------------------------------
 ;;-- init.javascript.skewer
